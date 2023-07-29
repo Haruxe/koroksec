c66ee319313b331e6a450356b97a2f2fd78dd16f
Haruxe <haruxe@proton.me>
2023-07-29 13:10:28 -0700

    fix typescript errors

--- pages/tools/Wallets.tsx
+++ pages/tools/Wallets.tsx
@@ -2,67 +2,92 @@ import React, { useEffect, useRef, useState } from "react";
 import { ethers } from "ethers";
 
 function WalletsTool() {
-  const [userInput, setUserInput] = useState();
-  const [result, setResult] = useState({'address': null, 'balance': null});
-  const [statusMessage, setStatusMessage] = useState({'opacity':'0', 'color':null, 'message': '.'});
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState({ address: "", balance: "" });
+  const [statusMessage, setStatusMessage] = useState({
+    opacity: "0",
+    color: "",
+    message: ".",
+  });
 
-  const getBalance = async (address) => {
-      const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth');
-      const balance = await provider.getBalance(address);
-      const balanceInEth = ethers.utils.formatEther(balance);
-      return balanceInEth;
+  const getBalance = async (address: any) => {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.ankr.com/eth"
+    );
+    const balance = await provider.getBalance(address);
+    const balanceInEth = ethers.utils.formatEther(balance);
+    return balanceInEth;
+  };
+
+  function handleInput(e: any) {
+    setUserInput(e.target.value);
+    setStatusMessage({ opacity: "0", color: "", message: "." });
   }
 
-  function handleInput(e){
-    setUserInput(e.target.value)
-    setStatusMessage({'opacity':'0', 'color':null, 'message': '.'});
-  }
-  function handleClic(){
+  function handleClick() {
     try {
       let _userInput = userInput;
-      if(!_userInput.startsWith('0x')){
-        _userInput = '0x'+ _userInput;
+      if (!_userInput.startsWith("0x")) {
+        _userInput = "0x" + _userInput;
       }
       let publicKey = ethers.utils.computeAddress(_userInput);
-      getBalance(publicKey).then( (balance) => {
-        setResult({'address': publicKey, 'balance': balance});
-      })
-    } catch(err){
-      setStatusMessage({'opacity':'100', 'color':'yellow-300', 'message':'Invalid private key'});
+      getBalance(publicKey).then((balance) => {
+        setResult({ address: publicKey, balance: balance });
+      });
+    } catch (err) {
+      setStatusMessage({
+        opacity: "100",
+        color: "yellow-300",
+        message: "Invalid private key",
+      });
     }
   }
 
-  useEffect(() => {
-  }, [userInput]);
+  useEffect(() => {}, [userInput]);
   return (
     <div className="select-none w-full h-full m-auto font-mono flex flex-col">
       <div className="flex flex-col justify-start gap-0">
         <h1 className="font-bold text-xl py-4">Private key to wallet</h1>
         <p className="text-sm leading-relaxed text-zinc-400">
-          Derive public key from a private key. 
+          Derive public key from a private key.
         </p>
-        <i className="text-xs text-zinc-400">Note: You should not trust a private key given by a public website for personal uses.</i>
+        <i className="text-xs text-zinc-400">
+          Note: You should not trust a private key given by a public website for
+          personal uses.
+        </i>
       </div>
       <div className="h-full w-full flex flex-col justify-center gap-y-8 items-center content-center">
         <div className="p-8 w-full flex flex-row justify-evenly gap-y-8 items-center content-center">
-          <input className="p-4 rounded-lg border border-1 border-white/50 w-3/6 bg-transparent" value={userInput} onChange={handleInput} />
-          <button className="text-sm p-4 rounded-lg border border-1 border-white/50 bg-white/20 hover:bg-white/30 active:bg-white/60 transition ease-in-out duration-300" onClick={handleClic}>
+          <input
+            className="p-4 rounded-lg border border-1 border-white/50 w-3/6 bg-transparent"
+            value={userInput}
+            onChange={handleInput}
+          />
+          <button
+            className="text-sm p-4 rounded-lg border border-1 border-white/50 bg-white/20 hover:bg-white/30 active:bg-white/60 transition ease-in-out duration-300"
+            onClick={handleClick}
+          >
             Compute public key
           </button>
         </div>
-        <p className={`text-sm text-${statusMessage.color} opacity-${statusMessage.opacity}`}>{statusMessage.message}</p>
-        <div className="w-3/6 flex flex-col justify-start gap-y-8 items-center content-center">
-          <span className="flex flex-col w-full gap-4">
-            <p>Public key:</p>
-            <p className="select-text">{result.address}</p>
-          </span>
-          <hr/>
-          <span className="flex flex-col w-full gap-4">
-            <p>Balance:</p>
-            <p>{result.balance} ETH</p>
-          </span>
-
-        </div>
+        <p
+          className={`text-sm text-${statusMessage.color} opacity-${statusMessage.opacity}`}
+        >
+          {statusMessage.message}
+        </p>
+        {result.address && (
+          <div className="w-3/6 flex flex-col justify-start gap-y-8 items-center content-center">
+            <span className="flex flex-col w-full gap-4">
+              <p>Public key:</p>
+              <p className="select-text">{result.address}</p>
+            </span>
+            <hr />
+            <span className="flex flex-col w-full gap-4">
+              <p>Balance:</p>
+              <p>{result.balance} ETH</p>
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -3,59 +3,55 @@ import ChecksumTool from "./Checksum";
 import WalletsTool from "./Wallets";
 import DummyTool from "./Dummy";
 
-function Home(props) {
-  const [activeTool, setActiveTool] = useState();  
+function Home() {
+  const [activeTool, setActiveTool] = useState<string | undefined>();
 
-  const buttonStyle = "select-none aspect-[16/4] px-4 rounded-r-sm border border-l-0 border-1 border-l-8 border-l-white/0 border-r-white hover:bg-white/40 hover:border-white/30 hover:border-l-white/60 transition ease-in-out duration-700 active:translate-x-1 active:duration-100";
-  const tools = [
-    'toCheckSumAddress()',
-    'PK_to_wallets()',
-    'Test 3',
-    'Test 4',
-  ];
+  const buttonStyle =
+    "select-none aspect-[16/4] px-4 rounded-r-sm border border-l-0 border-1 border-l-8 border-l-white/0 border-r-white hover:bg-white/40 hover:border-white/30 hover:border-l-white/60 transition ease-in-out duration-700 active:translate-x-1 active:duration-100";
+  const tools = ["toCheckSumAddress()", "PK_to_wallets()", "Test 3", "Test 4"];
 
-  function handleMenu(e){
-    setActiveTool(tools[e.target.value]);
+  function handleMenu(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (e.target instanceof HTMLButtonElement) {
+      setActiveTool(tools[parseInt(e.target.value)]);
+    }
   }
 
-  useEffect(() => {
-  }, [activeTool]);
+  useEffect(() => {}, [activeTool]);
   return (
     <>
       <div className="w-5/6 min-h-[88vh] mx-auto font-mono flex p-4 flex-row">
         <div className="grid grid-cols-1 content-center items-center justify-center gap-10 p-16">
-          {
-            tools.map( (tool, index) => {
-              return(
-                <button key={index} value={index} className={buttonStyle} onClick={handleMenu}>
-                  -{` ${tool}`}
-                </button>
-              )
-            })
-          }
+          {tools.map((tool, index) => {
+            return (
+              <button
+                key={index}
+                value={index}
+                className={buttonStyle}
+                onClick={handleMenu}
+              >
+                -{` ${tool}`}
+              </button>
+            );
+          })}
         </div>
         <div className="relative flex-1 place-content-center flex flex-col">
           <div className="absolute top-0">
             <p className="text-lg mb-10">
-              ~/pages/tools/{
-                activeTool != null
-                  ? activeTool
-                  : " "
-              }
+              ~/pages/tools/{activeTool != null ? activeTool : " "}
             </p>
           </div>
           <div className="w-full h-5/6">
-            {
-              tools.indexOf(activeTool) == 0
-              ? <ChecksumTool />
-              : tools.indexOf(activeTool) == 1
-                ? <WalletsTool />
-                : tools.indexOf(activeTool) == 2
-                  ? <DummyTool />
-                  : tools.indexOf(activeTool) == 3
-                    ? <DummyTool />
-                    : ''
-            }
+            {activeTool === tools[0] ? (
+              <ChecksumTool />
+            ) : activeTool === tools[1] ? (
+              <WalletsTool />
+            ) : activeTool === tools[2] ? (
+              <DummyTool />
+            ) : activeTool === tools[3] ? (
+              <DummyTool />
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
